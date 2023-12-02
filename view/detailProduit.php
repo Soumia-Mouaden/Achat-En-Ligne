03c59df6c52b0d5a8df68485b46466ced04854cd
oumaima <oumaimaoutchakoucht@gmail.com>
2023-12-02 13:23:29 +0100

    n

--- view/detailProduit.php
+++ view/detailProduit.php
@@ -515,9 +515,19 @@ $produits = $daoProduit->ProductsOfCategory($categoryOfProduct,$idOfProduit);
 
 
     <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
     <script src="js/main.js"></script>
 </body>
 
 </html>
+
+
+
+
