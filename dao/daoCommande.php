45d9b871b143c1b0e88f9a93253547cd93386fba
oumaima <oumaimaoutchakoucht@gmail.com>
2023-12-03 01:36:24 +0100

    s

--- dao/daoCommande.php
+++ dao/daoCommande.php
@@ -1,5 +1,7 @@
 <?php
 include "../model/commande.php";
+include "../model/produit.php";
+include "../model/commande_produit.php";
 
 class DaoCommande
 {
@@ -38,6 +40,7 @@ class DaoCommande
     //     $stm = $this->dbh->prepare("SELECT * FROM commande");
     //     $stm->execute();
 
+<<<<<<< HEAD
     //     $result = $stm->fetch(PDO::FETCH_ASSOC);
     //     if (!empty($result)) {
     //         $commande = new Commande(
@@ -50,6 +53,51 @@ class DaoCommande
     //     }
     //     return $commande;
     // }
+=======
+        $result = $stm->fetch(PDO::FETCH_ASSOC);
+        if (!empty($result)) {
+            $commande = new Commande(
+                $result['numCommande'],
+                $result['dateCreation'],
+                $result['dateLivraison'],
+                $result['etat'],
+                $result['villeLivraison'],
+                $result['adresse'],
+            );
+        }
+        return $commande;
+    }
+>>>>>>> 9c95e88fb7e124e7cc6b89f43b5d2cd8d067f56d
+
+    public function countCommandesToday()
+    {
+        $stm = $this->dbh->prepare("SELECT COUNT(*) as total FROM commande WHERE DATE(dateCreation) = DATE(NOW())");
+        $stm->execute();
+        $result = $stm->fetch(PDO::FETCH_ASSOC);
+        return $result['total'];
+    }
+
+    public function countCaisse()
+    {
+        $stm = $this->dbh->prepare("SELECT * 
+        FROM commande
+        JOIN commande_produit ON commande.numCommande = commande_produit.numCommande_Commande
+        JOIN produit ON commande_produit.id_Produit = produit.id
+        WHERE commande.etat = 'Livrée';");
+        $stm->execute();
+        $results = $stm->fetchAll(PDO::FETCH_ASSOC); // Utiliser fetchAll pour récupérer toutes les lignes
+    
+        $caisse = 0;
+        foreach ($results as $row) {
+            $caisse += ($row['prix'] * $row['quantité']); // Utiliser les clés du tableau associatif
+        }
+        return $caisse;
+    }
+    
+
+    
+
+
 
 }
 
